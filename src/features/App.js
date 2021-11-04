5015ab4a51ad36789bc028e70ee1e520ac9ef6c8
Suryacta <91002008+Suryacta@users.noreply.github.com>
2021-11-04 22:50:12 +0530

    Add files via upload

---
+++ src/features/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Homescreen from '../Homescreen';
+
+
+function App() {
+
+  
+  return (
+    <div className="App">
+     
+         
+      
+          
+            <Homescreen />
+        
+    </div>
+  );
+        
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCAkoWvyCmFinRyxGj8XlIQ_-ywxK_d1UM",
+  authDomain: "netflix-clone-c8f83.firebaseapp.com",
+  projectId: "netflix-clone-c8f83",
+  storageBucket: "netflix-clone-c8f83.appspot.com",
+  messagingSenderId: "681110942544",
+  appId: "1:681110942544:web:0cbcd4b59b7a713701f255"
+};
+
